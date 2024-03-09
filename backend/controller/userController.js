c678661ddfbaae753a7142aaf3e4ff892a20b88c
KavishkaSasindu <sasindu0215@gmail.com>
2024-03-09 19:02:48 +0530

    updated by ksdev

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -70,4 +70,60 @@ const getOneAdminData = async (request, response) => {
   }
 };
 
-module.exports = { createAdmin, getAdminData, getOneAdminData };
+// update admin data
+const updateAdminData = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const data = await UserModel.findByIdAndUpdate(id, request.body);
+    console.log(data);
+    if (data) {
+      const updated = await UserModel.findById(id);
+
+      return response.status(201).json({
+        message: "Updated Success",
+        data: updated,
+      });
+    }
+  } catch (error) {
+    if (error) {
+      return response.status(400).json({
+        message: error,
+      });
+    } else {
+      return response.status(404).json({
+        message: "Something went wrong",
+      });
+    }
+  }
+};
+
+// delete admin
+const deleteAdmin = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const deleteData = await UserModel.findByIdAndDelete(id);
+    if (!deleteData) {
+      return response.status(400).json({
+        message: "Not delete data",
+      });
+    } else {
+      return response.status(200).json({
+        message: "data delete Successfully",
+      });
+    }
+  } catch (error) {
+    if (error) {
+      return response.status(400).json({
+        message: error,
+      });
+    }
+  }
+};
+
+module.exports = {
+  createAdmin,
+  getAdminData,
+  getOneAdminData,
+  updateAdminData,
+  deleteAdmin,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,5 +6,8 @@ const router = express.Router();
 router.post("/api/admin", adminController.createAdmin);
 router.get("/", adminController.getAdminData);
 router.get("/api/:id", adminController.getOneAdminData);
+router.put("/api/updateAdmin/:id", adminController.updateAdminData);
+router.put("/api/updateAdmin/:id", adminController.updateAdminData);
+router.delete("/api/deleteAdmin/:id", adminController.deleteAdmin);
 
 module.exports = router;
